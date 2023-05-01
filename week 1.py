f5c3e6030d6418c0486f907cc7191778ff15e9c1
puja2504 <114426819+puja2504@users.noreply.github.com>
2023-05-01 18:02:16 +0530

    Delete week 1.py

--- week 1.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-weights = { f"w{i}" : np.round(np.random.random(), 2) for i in range(1, 7)}
-bias    = { f"b{i}" : np.round(np.random.random(), 2) for i in range(1, 4)}
-print (weights)
-print (bias)
