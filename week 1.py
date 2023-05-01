1a0c440f97e786c2063f0d0b7a1df848abd639ca
puja2504 <114426819+puja2504@users.noreply.github.com>
2023-05-01 20:59:39 +0530

    Update week 1.py

--- week 1.py
+++ week 1.py
@@ -1,14 +1,11 @@
 import numpy as np
- 
 input_size = 2
 hidden_size = 2
 output_size = 1
- 
 Weights1 = np.random.randn(input_size, hidden_size)
 biases1 = np.random.randn(hidden_size)
 Weights2 = np.random.randn(hidden_size, output_size)
 biases2 = np.random.randn(output_size)
- 
 print("Weights1 = ", Weights1)
 print("biases1 = ", biases1)
 print("Weights2 = ", Weights2)
