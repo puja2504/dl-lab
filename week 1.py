059b65ddb73810ceb663cef5d9b8ef2fa095ee34
puja2504 <114426819+puja2504@users.noreply.github.com>
2023-05-01 20:58:59 +0530

    Update week 1.py

--- week 1.py
+++ week 1.py
@@ -4,12 +4,12 @@ input_size = 2
 hidden_size = 2
 output_size = 1
  
-W1 = np.random.randn(input_size, hidden_size)
-b1 = np.random.randn(hidden_size)
-W2 = np.random.randn(hidden_size, output_size)
-b2 = np.random.randn(output_size)
+Weights1 = np.random.randn(input_size, hidden_size)
+biases1 = np.random.randn(hidden_size)
+Weights2 = np.random.randn(hidden_size, output_size)
+biases2 = np.random.randn(output_size)
  
-print("W1 = ", W1)
-print("b1 = ", b1)
-print("W2 = ", W2)
-print("b2 = ", b2)
+print("Weights1 = ", Weights1)
+print("biases1 = ", biases1)
+print("Weights2 = ", Weights2)
+print("biases2 = ", biases2)
